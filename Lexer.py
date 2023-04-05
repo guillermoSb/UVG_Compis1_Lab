b21d33fd53dd54e12c858cd4ebac0292fcd70d3e
Guillermo Santos <guillermo.santosba@gmail.com>
2023-04-05 07:42:32 -0600

    Finished range and pattern recognizion

--- Lexer.py
+++ Lexer.py
@@ -6,29 +6,31 @@ class Lexer():
 		
 		def parse_token(self, name, value):
 			new_value = ''
-			building_range = False
 			# Build the range
 			for i in range(len(value)):
 				if value[i] == "-":
-					building_range = True
 					start = ord(value[i-2])
 					end = ord(value[i+2])
 					for j in range(start, end + 1):
-						new_value += "{0:0=3d}".format(j)
+						new_value += "'{}'".format(chr(j))
 						if j != end:
 							new_value += "|"
-						
+			if new_value != '':
+				value = new_value	
+				new_value = ''
 			# Insert an | between each '' in the value
 			for i in range(len(value)):	
 				if i + 1 < len(value):
 						if value[i] == "'" and value[i + 1] == "'":
-							new_value += "|"						
-						
-				if i > 0 and i + 1 < len(value) and not building_range:
-					if value[i-1] == "'" and value[i+1] == "'":
+							new_value += "|"										
+				if i > 0 and i + 1 < len(value):
+					if value[i-1] == "'" and value[i+1] == "'" and value[i] != "|":
 						# Get ascii representation of the character
 						new_value += "{0:0=3d}".format(ord(value[i]))
+					elif value[i] == "|":
+						new_value += value[i]
 			# Replace [] with ()
+
 			new_value = "(" + new_value + ")"
 			return Token(name, new_value)
 
--- test_lexer.py
+++ test_lexer.py
@@ -15,3 +15,19 @@ def test_parse_token02():
 	token = lexer.parse_token('letra', "['a'-'d']")
 	# Assert
 	assert token.value == "(097|098|099|100)"
+
+def test_parse_token03():
+	# Arrange
+	lexer = Lexer()
+	# Act
+	token = lexer.parse_token('letra', "['a'-'d''A'-'D']")
+	# Assert
+	assert token.value == "(097|098|099|100|065|066|067|068)"
+
+def test_parse_token04():
+	# Arrange
+	lexer = Lexer()
+	# Act
+	token = lexer.parse_token('letra', "['a'-'d''A'-'D''0'-'9']")
+	# Assert
+	assert token.value == "(097|098|099|100|065|066|067|068|048|049|050|051|052|053|054|055|056|057)"
