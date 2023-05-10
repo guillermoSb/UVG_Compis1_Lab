11cb0986d3703f8795070090ad255306a50040cf
Guillermo Santos <guillermo.santosba@gmail.com>
2023-05-10 17:06:24 -0600

    Added examples

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ from Lexer import Lexer
 from Automata import Automata
 
 l = Lexer('yalex2.txt')
-a = l.create_automata()
-a.draw()
-# l.create_program()
+# a = l.create_automata()
+# a.draw()
+l.create_program()
