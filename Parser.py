0cba443ec7d76261f4d049a935d450ed064f572c
Guillermo Santos <guillermo.santosba@gmail.com>
2023-05-11 15:49:08 -0600

    Finished reading yapar

--- Parser.py
+++ Parser.py
@@ -15,9 +15,77 @@ class Grammar():
 				self.productions[self.start] = [self.start[0]]
 
 class Parser():
+
 	def __init__(self, grammar):
-		'''A Parser is used to parse a language. It takes a grammar as input and provides a parse method.'''
+		grammar.augment()
 		self.grammar = grammar
+	
+	@classmethod
+	def _from_file(cls, file_name):
+		file = open(file_name, 'r')
+		terminals = []
+		ignore = []
+		non_terminals = []
+		productions = {}
+
+		can_read_tokens = True
+		token_line = None
+		# Read the file line by line
+		for line in file:
+			line = line.strip()	# Remove leading and trailing whitespace			
+			if line.startswith('%token') and can_read_tokens:
+				terminals = terminals + line.split()[1:]
+			elif line.startswith('IGNORE'):
+				ignore = ignore + line.split()[1:]
+			elif line == "%%":
+				can_read_tokens = False
+				break;
+		
+		# Read the file line by line again to read the productions
+		is_making_production = False
+		current_production = ''
+		for line in file:
+			line = line.strip()
+			# Skip comments
+			if line.startswith('/*'):
+				continue
+			if not is_making_production:
+				for char in line:
+					if char == ":":
+						is_making_production = True
+						productions[current_production] = []
+						break
+					current_production += char
+			else:
+				if line == ";":
+					is_making_production = False
+					current_production = ''
+				else:
+					if line.startswith('|'):
+						line = line[1:]
+						line = line.strip()
+					productions[current_production].append(line)
+		
+		return Parser(Grammar(non_terminals, terminals, productions, list(productions.keys())[0]))
+
+			
+
+
+		
+
+
+				
+		
+
+			
+		
+			
+			
+				
+		
+		
+
+	
 
 	
 
--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from Lexer import Lexer
+from Parser import Parser
 from Automata import Automata
 
-l = Lexer('yalex2.txt')
-# a = l.create_automata()
-# a.draw()
-l.create_program()
+# l = Lexer('yalex2.txt')
+# # a = l.create_automata()
+# # a.draw()
+# l.create_program()
+
+parser = Parser._from_file('yapar.txt')
+
+print(parser.grammar.start)
